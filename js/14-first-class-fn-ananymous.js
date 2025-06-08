f5eb7fb8d000d50e1c68880a6a88ef4b983d9551
Harsh Kumar <harsh@notyouridea.com>
2025-06-09 00:27:37 +0530

    first class fn

--- js/14-first-class-fn-ananymous.js
+++ js/14-first-class-fn-ananymous.js
@@ -1,3 +1,52 @@
-function a() { //fn statement
+function a() { //fn statement or fn declaration
     console.log("a called...")
 }
+a();
+//we can aasign a fn to variable also //fn acts like a value
+var b = function () { //fn expression
+    console.log("b called")
+}
+b();
+
+
+
+//major diff bw fn statementa nd fn expression
+/*
+hoisting
+in fn statement, during memory creation phase, a is created in memory and fn is assigned.
+
+
+in case of fn expression, fn is treated like any other variable.
+it is undefined initially untill the code reaches this line
+
+
+
+Ananymous fn: a fn without name
+it doesnt have their own identity.
+
+function(){ //syntax error //as per eccma it should have name
+}
+
+//fn statements requires a function name
+
+
+use of it
+it is used where fn is used as values
+we cant use ananymous fn as function statement.
+
+
+we can assign the ananymous fn to a variable.
+
+
+var b = function xyz() { //xyz is not a function inside the outer scope
+    console.log("B called")
+}
+xyz() //uncaught ReferenceError: xyz is not defined
+
+
+//parameters and arguments
+var b = function (a, b) { //a and b is parameter //local variable in fn scope //value received by fn in local scope is parameter.
+    console.log(a + b)
+}
+b(2, 4); //value passed to fn is arguments
+*/
